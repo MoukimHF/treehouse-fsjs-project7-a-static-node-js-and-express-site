48da13af965b68f8ed7dd0952f768457240a1ef8
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2018-10-07 04:37:53 +0200

    add comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data/data").projects;
 
+// root route
 router.get("/", (req, res) => {
   res.render("index", { data });
 });
 
+// route for about page
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
+// route to display one of the selected projects
 router.get("/projects/:projectId", (req, res) => {
   res.render("project", { project: data[parseInt(req.params.projectId)] });
 });
